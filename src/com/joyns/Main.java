58da834328aff0a765cb093e64af4fbf4ea491ff
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-07-28 23:56:09 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/joyns/Main.java
@@ -0,0 +1,16 @@
+// Java program to demonstrate java.util.String joyn() method with specific separator
+package com.joyns;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Creating string with parameters: separator, list of elements
+       String fruits = String.join(" ","Mango", "Melon", "Cherry");
+
+       // Printing string with name fruits to console
+       System.out.println(fruits);
+    }
+}
